1c2947c79e28dc215eb29e1c3bf3eb7e3f661cf7
kirill.leibov@dxc.com <kirill.leibov@dxc.com>
2021-08-17 14:04:37 +0200

    add the overwrite flag as start arg

--- Scripts/from-mp3.cmd
+++ Scripts/from-mp3.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+rem SET "FFMPEG=..\_tools\ffmpeg-n4.4-79-gde1132a891-win64-gpl-4.4\bin\ffmpeg.exe"
 SET "FFMPEG=..\_tools\ffmpeg-2021-01-05-git-66deab3a26-full_build\bin\ffmpeg.exe"
 
 
@@ -7,6 +8,8 @@ SET "FFMPEG=..\_tools\ffmpeg-2021-01-05-git-66deab3a26-full_build\bin\ffmpeg.exe
 
 IF [%1]==[] ECHO Please set the audio file name && GOTO END
 IF [%2]==[] ECHO Please set the audio file format (MP3 or WAV) && GOTO END
+IF [%3]==[-y] SET OVERWRITE=-y
+
 SET "EXT=%2"
 
 SET "INPUT_PATH=..\_audio"
@@ -14,9 +17,16 @@ SET "OUTPUT_PATH=..\ProsePlayer\public\media"
 
 IF NOT EXIST %INPUT_PATH%\%1.%EXT% ECHO File not found: '%INPUT_PATH%\%1.%EXT%' && GOTO END
 
-%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -vn -dash 1 %INPUT_PATH%\%1.webm
-%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -vn -codec:a aac %INPUT_PATH%\%1.m4a
-%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -af aformat=s16:16000 -ac 1 %INPUT_PATH%\%1.flac
+:WEBM
+echo *** Convert to WEBM
+%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -vn -dash 1 %INPUT_PATH%\%1.webm %OVERWRITE%
+:M4A
+echo *** Convert to M4A
+%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -vn -codec:a aac %INPUT_PATH%\%1.m4a %OVERWRITE%
+:FLAC
+echo *** Convert to FLAC
+%FFMPEG% -i %INPUT_PATH%\%1.%EXT% -af aformat=s16:16000 -ac 1 -start_at_zero -copytb 1 %INPUT_PATH%\%1.flac  %OVERWRITE%
+
 COPY %INPUT_PATH%\%1.flac %OUTPUT_PATH%
 COPY %INPUT_PATH%\%1.m4a %OUTPUT_PATH%
 COPY %INPUT_PATH%\%1.webm %OUTPUT_PATH%
