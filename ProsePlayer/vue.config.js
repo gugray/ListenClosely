12e1c6b64832c212e3e3760d3dd23b3d61ef5ba2
gugray <gabor@berlin.de>
2021-10-25 15:45:09 +0200

    player: public path config simplified

--- ProsePlayer/vue.config.js
+++ ProsePlayer/vue.config.js
@@ -1,8 +1,8 @@
 ﻿// vue.config.js
 module.exports = {
   indexPath: 'player.html',
-  //publicPath: process.env.NODE_ENV === 'production' ? '/prose/' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: '/',
+  //publicPath: '/prose',
   //chainWebpack: config => {
   //  config.module.rules.delete('eslint');
   //},
